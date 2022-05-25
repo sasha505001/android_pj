9bace70cf4947ae710538af89bf6cccdb6681bf3
astre <astrelin2001@gmail.com>
2022-05-25 22:15:56 +0300

    Красивая кнопка меню и кнопка добавить в меню

--- app/src/main/java/com/example/trainer_helper_and_eat_supplements/activity_list.kt
+++ app/src/main/java/com/example/trainer_helper_and_eat_supplements/activity_list.kt
@@ -2,10 +2,36 @@ package com.example.trainer_helper_and_eat_supplements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.trainer_helper_and_eat_supplements.databinding.ActivityListBinding
 
 class activity_list : AppCompatActivity() {
+
+    // объект для обращения к элементам экрана
+    lateinit var binding: ActivityListBinding
+
+    // Меню
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
+
+        // Заполнение объекта экрана
+        binding = ActivityListBinding.inflate(layoutInflater)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, binding.mainDrawerLayout, R.string.nav_open, R.string.nav_close)
+        binding.mainDrawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setContentView(binding.root)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_menu, menu)
+        return true
+    }
+
+
 }
