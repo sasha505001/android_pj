b2edf3221013ede76fa359d6e184b763f941eeec
astre <astrelin2001@gmail.com>
2022-05-25 22:08:24 +0300

    Добавил меню, добавил разметку для списков

---
+++ app/src/main/java/com/example/trainer_helper_and_eat_supplements/activity_list.kt
@@ -0,0 +1,11 @@
+package com.example.trainer_helper_and_eat_supplements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
