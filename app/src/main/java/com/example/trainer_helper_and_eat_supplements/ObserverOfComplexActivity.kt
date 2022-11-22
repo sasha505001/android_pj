1a9576e302d99546de4a92da9d5bd69cf142b3b3
astre <astrelin2001@gmail.com>
2022-11-22 16:24:21 +0300

    Начал работать над отображением комплекса упражнений

---
+++ app/src/main/java/com/example/trainer_helper_and_eat_supplements/ObserverOfComplexActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trainer_helper_and_eat_supplements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ObserverOfComplexActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.observer_of_complex_activity)
+    }
+}
